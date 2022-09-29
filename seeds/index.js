3a033d2cd856e9fe14a6feea15e9f3303c5b415e
Alexandre Arantes <alexandrearantes@Alexandres-MacBook-Pro.local>
2022-09-29 20:32:09 +0100

    Added Geocoding and map capabilities to Campgrounds

--- seeds/index.js
+++ seeds/index.js
@@ -15,6 +15,27 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+const images = [{
+                    url: 'https://res.cloudinary.com/dguk8rvgh/image/upload/v1664456165/YelpCamp/nyadltnxy47jgra3h55y.jpg',
+                    filename: 'YelpCamp/nyadltnxy47jgra3h55y'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dguk8rvgh/image/upload/v1664456168/YelpCamp/cjedbnhc8osrbsa8othk.jpg',
+                    filename: 'YelpCamp/cjedbnhc8osrbsa8othk'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dguk8rvgh/image/upload/v1664456170/YelpCamp/ypthm0frkldqblb9k07x.jpg',
+                    filename: 'YelpCamp/ypthm0frkldqblb9k07x'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dguk8rvgh/image/upload/v1664456172/YelpCamp/imoe22ualy4yqevj8up6.jpg',
+                    filename: 'YelpCamp/imoe22ualy4yqevj8up6'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dguk8rvgh/image/upload/v1664456176/YelpCamp/fl9gtn1dvwqrjsa5fq52.jpg',
+                  filename: 'YelpCamp/fl9gtn1dvwqrjsa5fq52'
+                }];
+
 const sample = array => array[Math.floor(Math.random()* array.length)];
 
 const seedDB = async () => {
@@ -29,28 +50,8 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo sunt ipsum fugit quo. Reiciendis corrupti nemo omnis cumque ex, recusandae provident, ab cupiditate voluptates, quibusdam quidem dolor officia? Repudiandae, voluptates.',
             price: price,
-            images: [
-                {
-                    url: 'https://res.cloudinary.com/dguk8rvgh/image/upload/v1664456165/YelpCamp/nyadltnxy47jgra3h55y.jpg',
-                    filename: 'YelpCamp/nyadltnxy47jgra3h55y'
-                  },
-                  {
-                    url: 'https://res.cloudinary.com/dguk8rvgh/image/upload/v1664456168/YelpCamp/cjedbnhc8osrbsa8othk.jpg',
-                    filename: 'YelpCamp/cjedbnhc8osrbsa8othk'
-                  },
-                  {
-                    url: 'https://res.cloudinary.com/dguk8rvgh/image/upload/v1664456170/YelpCamp/ypthm0frkldqblb9k07x.jpg',
-                    filename: 'YelpCamp/ypthm0frkldqblb9k07x'
-                  },
-                  {
-                    url: 'https://res.cloudinary.com/dguk8rvgh/image/upload/v1664456172/YelpCamp/imoe22ualy4yqevj8up6.jpg',
-                    filename: 'YelpCamp/imoe22ualy4yqevj8up6'
-                  },
-                  {
-                    url: 'https://res.cloudinary.com/dguk8rvgh/image/upload/v1664456176/YelpCamp/fl9gtn1dvwqrjsa5fq52.jpg',
-                    filename: 'YelpCamp/fl9gtn1dvwqrjsa5fq52'
-                  }
-            ]
+            images: images[Math.floor(Math.random()*5)],
+            geometry: { type: 'Point', coordinates: [ Math.random() * -2 - 6.103721, Math.random()  * 2 + 53.295386 ] }
         })
         await camp.save();
     }
